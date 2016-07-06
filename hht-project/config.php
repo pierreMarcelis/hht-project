ceae574f8a10ff7d90c0b3bab09836517aa11f00
pierreMarcelis <pierre.marcelis@gmail.com>
2016-07-06 21:54:55 +0200

    login.php

--- hht-project/config.php
+++ hht-project/config.php
@@ -3,12 +3,10 @@
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', '');
    define('DB_DATABASE', 'hhtdocuments');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   
-  
-if (!$db) {
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-
+   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+ 
+// Check connection
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
 ?>
--- hht-project/login.php
+++ hht-project/login.php
@@ -1,43 +1,81 @@
  <?php
 include("config.php");
 session_start();
-echo "Hello BEFORE POST";
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
-// username and password sent from Form
-$myusername=mysqli_real_escape_string($db,$_POST['username']); 
-$mypassword=mysqli_real_escape_string($db,$_POST['password']); 
-$passwordSecure=md5($mypassword);
+// escape variables for security
+$myusername = mysqli_real_escape_string($con, $_POST['username']);
+$password = mysqli_real_escape_string($con, $_POST['password']);
+
 $sql="SELECT * FROM HHT_USERS where EMAIL= '$myusername' and PASSWORD='$mypassword'";
 
-echo "Hello BEFORE CALL SQL \n"; 
-echo $sql;
-$request=mysqli_query($db,$sql)  or die(mysqli_error($db));
-echo "Hello AFTER CALL SQL \n"; 
-$rows = mysql_num_rows($query) or die($query);
-echo "Result Size : ";
-echo $rows;
-if ($rows == 1) {
-	$_SESSION['login_user']=$username; // Initializing Session
-	
-	 $login_session = $row['EMAIL'];
-	  echo $row['EMAIL']; 
-	
-   echo $login_session;
-	header("location: welcome.php"); // Redirecting To Other Page
-} else {
-	$error = "Username or Password is invalid";
+
+if (!mysqli_query($con,$sql)) {
+  die('Error: ' . mysqli_error($con));
 }
-echo "Hello BEFORE CLOSING CONNEXION";
+
+$result = $mysqli->query ($sql);
+
+   $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+
+
 mysql_close($connection); // Closing Connection
-}
-echo "Hello END";
 
 ?>
-<form method="post">
-<label>UserName :</label>
-<input type="text" name="username"/><br />
-<label>Password :</label>
-<input type="password" name="password"/><br/>
-<input type="submit" value=" Submit "/><br />
-</form>
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <style type = "text/css">
+         body {
+            font-family:Arial, Helvetica, sans-serif;
+            font-size:14px;
+         }
+         
+         label {
+            font-weight:bold;
+            width:100px;
+            font-size:14px;
+         }
+         
+         .box {
+            border:#666666 solid 1px;
+         }
+      </style>
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+					
+            </div>
+				
+         </div>
+			
+      </div>
+
+   </body>
+</html>
