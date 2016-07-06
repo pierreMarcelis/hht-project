d9c9075948bfc06e77a994c07ff6ec04a7ab16c0
pierreMarcelis <pierre.marcelis@gmail.com>
2016-07-07 01:24:57 +0200

    updating login process

--- hht-project/config.php
+++ hht-project/config.php
@@ -1,12 +1,15 @@
 <?php
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
+   define('DB_PASSWORD', ' ');
    define('DB_DATABASE', 'hhtdocuments');
-   $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = @mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
  
 // Check connection
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
+       
     }
+   
+   
 ?>
--- hht-project/login.php
+++ hht-project/login.php
@@ -1,23 +1,34 @@
 <?php
 include("config.php");
+    
 session_start();
 $error = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
 // escape variables for security
-    $myusername = mysqli_real_escape_string($con, $_POST['username']);
-    $password = mysqli_real_escape_string($con, $_POST['password']);
-
-    $sql = "SELECT * FROM HHT_USERS where EMAIL= '$myusername' and PASSWORD='$mypassword'";
+    $myusername = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
 
 
-    if (!mysqli_query($con, $sql)) {
-        die('Error: ' . mysqli_error($con));
-    }
 
-    $result = $mysqli->query($sql);
+// Building the sql query
+    $sql = "SELECT * FROM HHT_USERS where EMAIL= '$myusername' and PASSWORD='$password'";
 
-    $count = mysqli_num_rows($result);
-
+// on envoie la requête 
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());  
+$countResult=0;
+// on fait une boucle qui va faire un tour pour chaque enregistrement 
+while($data = mysql_fetch_array($req))  
+    { 
+    // on affiche les informations de l'enregistrement en cours 
+    echo '<b>'.$data['ID'].' '.$data['FIRST_NAME'].$data['FIRST_NAME'].$data['LAST_NAME'].$data['EMAIL'].$data['HHT_ROLE'].'</b> ('.$data['PASSWORD'].')';  
+    $countResult = $countResult+1;
+    }  
+    echo $countResult;
+// on ferme la connexion à mysql 
+mysql_close($db);  
+    
+    
     // If result matched $myusername and $mypassword, table row must be 1 row
 
     if ($count == 1) {
@@ -27,9 +38,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("location: welcome.php");
     } else {
         $error = "Your Login Name or Password is invalid";
+        header("location: index.php");
     }
-
-
-    mysql_close($connection); // Closing Connection
 }
 ?>
