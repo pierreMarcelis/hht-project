5c7ad42eecbd45d2101bb409c9a84d947ffb936d
pierreMarcelis <pierre.marcelis@gmail.com>
2016-08-03 22:43:29 +0200

    update

--- hht-project/config.php
+++ hht-project/config.php
@@ -1,9 +1,9 @@
 <?php
    define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', ' ');
+   define('DB_PASSWORD', '');
    define('DB_DATABASE', 'hhtdocuments');
-   $db = @mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
  
 // Check connection
     if (mysqli_connect_errno()) {
--- hht-project/login.php
+++ hht-project/login.php
@@ -1,44 +1,90 @@
 <?php
-include("config.php");
+
+//::if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
+//// GET post values
+ //   $myusername = isset($_POST['username']) ? $_POST['username'] : '';
+  //  $password = isset($_POST['password']) ? $_POST['password'] : '';
+ 
+//Connection creation
+// $db_conn = new mysqli('localhost', 'root', '', 'hhtdocuments');
+
+//if(mysqli_connect_errno()) {
+ //   echo 'Connection to the database failed'.mysqli_connect_error();
+ //s   exit;
+// ::s}
+// $query = 'SELECT * FROM HHT_USERS ' 
+ //        ."where EMAIL='$myusername' " 
+ //s        ." and PASSWORD='$password')";
+
+//while ($row = $query->fetch_assoc()) {
+   
+//    $_SESSION['username']=$row['EMAIL'];
+//    $_SESSION['role']=$row['HHT_ROLE'];
+//}
+//}
+
+
+
 session_start();
-$error = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-// escape variables for security
-    $myusername = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
+include("config.php"); //Establishing connection with our database
+ 
+$error = ""; //Variable for storing our errors.
+if(isset($_POST["submit"]))
+{
+if(empty($_POST["username"]) || empty($_POST["password"]))
+{
+$error = "Both fields are required.";
+}else
+{
+// Define $username and $password
+$username=$_POST['username'];
+$password=$_POST['password'];
+ 
+// To protect from MySQL injection
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysqli_real_escape_string($db, $username);
+$password = mysqli_real_escape_string($db, $password);
+//$password = md5($password);
+ 
+//Check username and password from database
+$sql="SELECT * FROM HHT_USERS WHERE EMAIL='$username' and PASSWORD='$password')";
 
 
+$result=mysqli_query($db,$sql);
 
-// Building the sql query
-    $sql = "SELECT * FROM HHT_USERS where EMAIL= '$myusername' and PASSWORD='$password'";
+if ($result == false) {
+    $errorMessage = "The query failed to execute : \n"
+                   . "Your Query: ".$sql."\n"
+                   . " Error: (errno:".mysql_errno().") Error : ".mysql_error();
+				  
+    die($errorMessage);
+} else {
+	
+//$row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+ $tab= mysqli_fetch_array($result);
+foreach ($tab as $row)  {
+	echo "Coucou";
+	$login_user = isset($row['EMAIL']) ? $row['EMAIL'] : null;
+	echo $login_user;
+	// Initializing Session
+	$_SESSION['username'] = isset($row['EMAIL']) ? $row['EMAIL'] : null;
+}
+//If username and password exist in our database then create a session.
+//Otherwise echo error.
+ 
+if(empty($_SESSION['username']))
+{
 
-// on envoie la requête 
-$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());  
-$countResult=0;
-// on fait une boucle qui va faire un tour pour chaque enregistrement 
-while($data = mysql_fetch_array($req))  
-    { 
-    // on affiche les informations de l'enregistrement en cours 
-    echo '<b>'.$data['ID'].' '.$data['FIRST_NAME'].$data['FIRST_NAME'].$data['LAST_NAME'].$data['EMAIL'].$data['HHT_ROLE'].'</b> ('.$data['PASSWORD'].')';  
-    $countResult = $countResult+1;
-    }  
-    echo $countResult;
-// on ferme la connexion à mysql 
-mysql_close($db);  
-    
-    
-    // If result matched $myusername and $mypassword, table row must be 1 row
-
-    if ($count == 1) {
-        session_register("myusername");
-        $_SESSION['login_user'] = $myusername;
-
-        header("location: welcome.php");
-    } else {
-        $error = "Your Login Name or Password is invalid";
-        header("location: index.php");
-    }
+$error = "Incorrect username or password.";
+}else
+{
+ 
+header("location: home.php"); // Redirecting To Other Page
+}
+ 
+}
+}
 }
 ?>
