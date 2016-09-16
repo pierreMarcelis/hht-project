576795bacfb766b48cb29f28a5aa46827d570792
pierreMarcelis <pierre.marcelis@gmail.com>
2016-09-16 22:17:45 +0200

    new structure

---
+++ hht-project/documents_page.php
@@ -0,0 +1,6 @@
+<?php
+    require_once ('data_validator.php');
+    require_once ('db_init.php');
+    require_once ('authentification_user.php');
+    require_once ('output_display.php');
+?>
---
+++ hht-project/login.php
@@ -0,0 +1,8 @@
+<?php
+require_once('documents_page.php');
+do_html_header('');
+
+display_site_info();
+display_login_form();
+do_html_footer();
+?>
