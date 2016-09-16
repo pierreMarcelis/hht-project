2d36e61b7c19aa6e399f485a47393de3911471cc
pierreMarcelis <pierre.marcelis@gmail.com>
2016-09-17 00:13:05 +0200

    updating database

--- hht-project/data_validator.php
+++ hht-project/data_validator.php
@@ -1,16 +1,5 @@
 <?php
 
-function filled_out($form_vars)
-{
-    // test that each variable has a value
-    foreach ($form_vars as $key => $value)
-    {
-        if (!isset($key) || ($value == ''))
-            return false;
-    }
-    return true;
-}
-
 function valid_email($address)
 {
     // check an email address is possibly valid
