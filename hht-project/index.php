04e6dd8e1b01fe76c8a5c4ca5d0764a7a593538e
pierreMarcelis <pierre.marcelis@gmail.com>
2017-02-14 18:59:20 +0100

    quick fix

--- hht-project/index.php
+++ hht-project/index.php
@@ -1,20 +1,19 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		
-	<title>Humanity Help Team Intranet</title>
-    <script src="./jquery/jquery-3.1.1.min.js" ></script>
-	<link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
-	<link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap-theme.min.css" />
-    <script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"</script>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 
+    <title>Humanity Help Team Intranet</title>
+
+    <link rel="stylesheet" href="./bootstrap-3.3.7-dist//css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="./bootstrap-3.3.7-dist//css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">;
+    <script src="./jquery/jquery-3.1.1.min.js"></script>
+    <script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<body>
+<div>
+    <?php include 'login.php'?>
+</div>
+</body>
 </head>
-	<body>
-		<div>
-			<?php include 'login.php'?>
-		</div>
-	</body>
-
 </html>
 
